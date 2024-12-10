f2976dd02df6657eb7fc07adf8d383a05df876f3
EllieNtasio <ellintasio@gmail.com>
2024-12-10 19:52:05 +0200

    almost finished

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 
 
+export default function GameBoard({onSelectSquare,board}){
 
-const initialGameBoard = [
-    [null, null,null],
-    [null, null,null],
-    [null, null,null]
-];
-
-export default function GameBoard({onSelectSquare,activePlayerSymbol}){
-    const [gameBoard,setGameBoard] = useState(initialGameBoard);
+    
+  /*  const [gameBoard,setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex,colIndex){
         setGameBoard((prevGameBoard)=>{
@@ -19,15 +13,15 @@ export default function GameBoard({onSelectSquare,activePlayerSymbol}){
         });
 
         onSelectSquare();
-    }
+    }*/
 
 
     return (
         <ol id='game-board'>
-            {gameBoard.map((row ,rowIndex)=> <li key={rowIndex}>
+            {board.map((row ,rowIndex)=> <li key={rowIndex}>
                 <ol>
                  {row.map((playerSymbol,colIndex)=> <li key={colIndex}>  
-                    <button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                    <button onClick={()=>onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol!== null}>{playerSymbol}</button>
                      </li>  )}   
                 </ol> 
             </li>)}
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,8 @@
+export default function GameOver({winner,onRestart}){
+    return <div id="game-over">
+        <h2>GAME OVER!</h2>
+         {winner && <p>{winner} WON! </p>}
+         {!winner && <p>ITS A DRAW </p>}
+        <p><button onClick={onRestart}>REMATCH!</button></p>
+    </div>
+}
